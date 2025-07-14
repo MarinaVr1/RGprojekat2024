5bff828fafa6e2c018ea1f461fbe1876c45c5167
marina vracaric <marina.vracaric03@gmail.com>
2025-07-14 19:23:47 +0200

    MainController with functions for moving using keybord and mouse

---
+++ app/include/MainController.hpp
@@ -0,0 +1,29 @@
+#ifndef MAINCONTROLLER_HPP
+#define MAINCONTROLLER_HPP
+#include <engine/core/Controller.hpp>
+
+namespace app {
+class MainController : public engine::core::Controller {
+    void initialize() override;
+
+    bool loop() override;
+
+    void draw_busStop();
+
+    void begin_draw() override;
+
+    void update_camera();
+
+    void update() override;
+
+    void draw() override;
+
+    void end_draw() override;
+
+public:
+    std::string_view name() const override { return "app::MainController"; }
+
+};
+}
+
+#endif //MAINCONTROLLER_HPP
