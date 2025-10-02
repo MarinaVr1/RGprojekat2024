793d523c8065e9cf6e9938204734adf22f923646
marina vracaric <marina.vracaric03@gmail.com>
2025-10-02 02:00:14 +0200

    CHECKED_GL_CALL added to every gl function

--- engine/src/Bloom.cpp
+++ engine/src/Bloom.cpp
@@ -1,4 +1,5 @@
 #include <engine/graphics/Bloom.hpp>
+#include <engine/graphics/OpenGL.hpp>
 #include <engine/platform/PlatformController.hpp>
 #include <glad/glad.h>
 #include <spdlog/spdlog.h>
@@ -8,19 +9,19 @@ using namespace engine::graphics;
 Bloom::Bloom() = default;
 
 Bloom::~Bloom() {
-    glDeleteFramebuffers(1, &hdrFBO);
-    glDeleteTextures(2, colorBuffers);
-    glDeleteRenderbuffers(1, &rboDepth);
+    CHECKED_GL_CALL(glDeleteFramebuffers, 1, &hdrFBO);
+    CHECKED_GL_CALL(glDeleteTextures, 2, colorBuffers);
+    CHECKED_GL_CALL(glDeleteRenderbuffers, 1, &rboDepth);
 
     for (int i = 0; i < 2; ++i) {
-        glDeleteFramebuffers(1, &pingpongFBO[i]);
-        glDeleteTextures(1, &pingpongColorBuffers[i]);
+        CHECKED_GL_CALL(glDeleteFramebuffers, 1, &pingpongFBO[i]);
+        CHECKED_GL_CALL(glDeleteTextures, 1, &pingpongColorBuffers[i]);
     }
 
     if (quadVAO)
-        glDeleteVertexArrays(1, &quadVAO);
+        CHECKED_GL_CALL(glDeleteVertexArrays, 1, &quadVAO);
     if (quadVBO)
-        glDeleteBuffers(1, &quadVBO);
+        CHECKED_GL_CALL(glDeleteBuffers, 1, &quadVBO);
 }
 
 void Bloom::init(int screenWidth, int screenHeight,
@@ -31,42 +32,42 @@ void Bloom::init(int screenWidth, int screenHeight,
     shaderBlur_ = shaderBlur;
     shaderFinal_ = shaderFinal;
 
-    glGenFramebuffers(1, &hdrFBO);
-    glBindFramebuffer(GL_FRAMEBUFFER, hdrFBO);
+    CHECKED_GL_CALL(glGenFramebuffers, 1, &hdrFBO);
+    CHECKED_GL_CALL(glBindFramebuffer, GL_FRAMEBUFFER, hdrFBO);
 
-    glGenTextures(2, colorBuffers);
+    CHECKED_GL_CALL(glGenTextures, 2, colorBuffers);
     for (unsigned int i = 0; i < 2; i++) {
-        glBindTexture(GL_TEXTURE_2D, colorBuffers[i]);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, screenWidth, screenHeight, 0, GL_RGBA, GL_FLOAT, nullptr);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + i, GL_TEXTURE_2D, colorBuffers[i], 0);
+        CHECKED_GL_CALL(glBindTexture, GL_TEXTURE_2D, colorBuffers[i]);
+        CHECKED_GL_CALL(glTexImage2D, GL_TEXTURE_2D, 0, GL_RGBA16F, screenWidth, screenHeight, 0, GL_RGBA, GL_FLOAT, nullptr);
+        CHECKED_GL_CALL(glTexParameteri, GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        CHECKED_GL_CALL(glTexParameteri, GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        CHECKED_GL_CALL(glTexParameteri, GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        CHECKED_GL_CALL(glTexParameteri, GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        CHECKED_GL_CALL(glFramebufferTexture2D, GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + i, GL_TEXTURE_2D, colorBuffers[i], 0);
     }
 
     unsigned int attachments[2] = {GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1};
-    glDrawBuffers(2, attachments);
+    CHECKED_GL_CALL(glDrawBuffers, 2, attachments);
 
-    glGenRenderbuffers(1, &rboDepth);
-    glBindRenderbuffer(GL_RENDERBUFFER, rboDepth);
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, screenWidth, screenHeight);
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, rboDepth);
+    CHECKED_GL_CALL(glGenRenderbuffers, 1, &rboDepth);
+    CHECKED_GL_CALL(glBindRenderbuffer, GL_RENDERBUFFER, rboDepth);
+    CHECKED_GL_CALL(glRenderbufferStorage, GL_RENDERBUFFER, GL_DEPTH_COMPONENT, screenWidth, screenHeight);
+    CHECKED_GL_CALL(glFramebufferRenderbuffer, GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, rboDepth);
 
 
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    CHECKED_GL_CALL(glBindFramebuffer, GL_FRAMEBUFFER, 0);
 
-    glGenFramebuffers(2, pingpongFBO);
-    glGenTextures(2, pingpongColorBuffers);
+    CHECKED_GL_CALL(glGenFramebuffers, 2, pingpongFBO);
+    CHECKED_GL_CALL(glGenTextures, 2, pingpongColorBuffers);
     for (unsigned int i = 0; i < 2; i++) {
-        glBindFramebuffer(GL_FRAMEBUFFER, pingpongFBO[i]);
-        glBindTexture(GL_TEXTURE_2D, pingpongColorBuffers[i]);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, screenWidth, screenHeight, 0, GL_RGBA, GL_FLOAT, NULL);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, pingpongColorBuffers[i], 0);
+        CHECKED_GL_CALL(glBindFramebuffer, GL_FRAMEBUFFER, pingpongFBO[i]);
+        CHECKED_GL_CALL(glBindTexture, GL_TEXTURE_2D, pingpongColorBuffers[i]);
+        CHECKED_GL_CALL(glTexImage2D, GL_TEXTURE_2D, 0, GL_RGBA16F, screenWidth, screenHeight, 0, GL_RGBA, GL_FLOAT, nullptr);
+        CHECKED_GL_CALL(glTexParameteri, GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        CHECKED_GL_CALL(glTexParameteri, GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        CHECKED_GL_CALL(glFramebufferTexture2D, GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, pingpongColorBuffers[i], 0);
 
-        if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) { spdlog::error("Pingpong FBO is not complete", i); }
+        if (CHECKED_GL_CALL(glCheckFramebufferStatus, GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) { spdlog::error("Pingpong FBO is not complete", i); }
     }
 
     shaderBlur_->use();
@@ -87,26 +88,26 @@ void Bloom::renderQuad() {
                 1.0f, -1.0f, 1.0f, 0.0f,
         };
 
-        glGenVertexArrays(1, &quadVAO);
-        glGenBuffers(1, &quadVBO);
+        CHECKED_GL_CALL(glGenVertexArrays, 1, &quadVAO);
+        CHECKED_GL_CALL(glGenBuffers, 1, &quadVBO);
 
-        glBindVertexArray(quadVAO);
-        glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
+        CHECKED_GL_CALL(glBindVertexArray, quadVAO);
+        CHECKED_GL_CALL(glBindBuffer, GL_ARRAY_BUFFER, quadVBO);
+        CHECKED_GL_CALL(glBufferData, GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
 
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *) 0);
+        CHECKED_GL_CALL(glEnableVertexAttribArray, 0);
+        CHECKED_GL_CALL(glVertexAttribPointer, 0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *) 0);
 
-        glEnableVertexAttribArray(1);
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *) (2 * sizeof(float)));
+        CHECKED_GL_CALL(glEnableVertexAttribArray, 1);
+        CHECKED_GL_CALL(glVertexAttribPointer, 1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *) (2 * sizeof(float)));
 
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-        glBindVertexArray(0);
+        CHECKED_GL_CALL(glBindBuffer, GL_ARRAY_BUFFER, 0);
+        CHECKED_GL_CALL(glBindVertexArray, 0);
     }
 
-    glBindVertexArray(quadVAO);
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-    glBindVertexArray(0);
+    CHECKED_GL_CALL(glBindVertexArray, quadVAO);
+    CHECKED_GL_CALL(glDrawArrays, GL_TRIANGLE_STRIP, 0, 4);
+    CHECKED_GL_CALL(glBindVertexArray, 0);
 }
 
 void Bloom::render() {
@@ -115,23 +116,23 @@ void Bloom::render() {
 
     shaderBlur_->use();
     for (int i = 0; i < amount; i++) {
-        glBindFramebuffer(GL_FRAMEBUFFER, pingpongFBO[horizontal]);
+        CHECKED_GL_CALL(glBindFramebuffer, GL_FRAMEBUFFER, pingpongFBO[horizontal]);
         shaderBlur_->set_bool("horizontal", horizontal);
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, first_iteration ? colorBuffers[1] : pingpongColorBuffers[!horizontal]);
+        CHECKED_GL_CALL(glActiveTexture, GL_TEXTURE0);
+        CHECKED_GL_CALL(glBindTexture, GL_TEXTURE_2D, first_iteration ? colorBuffers[1] : pingpongColorBuffers[!horizontal]);
         renderQuad();
         horizontal = !horizontal;
         if (first_iteration) first_iteration = false;
     }
 
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    CHECKED_GL_CALL(glBindFramebuffer, GL_FRAMEBUFFER, 0);
+    CHECKED_GL_CALL(glClear, GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     shaderFinal_->use();
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, colorBuffers[0]);
-    glActiveTexture(GL_TEXTURE1);
-    glBindTexture(GL_TEXTURE_2D, pingpongColorBuffers[!horizontal]);
+    CHECKED_GL_CALL(glActiveTexture, GL_TEXTURE0);
+    CHECKED_GL_CALL(glBindTexture, GL_TEXTURE_2D, colorBuffers[0]);
+    CHECKED_GL_CALL(glActiveTexture, GL_TEXTURE1);
+    CHECKED_GL_CALL(glBindTexture, GL_TEXTURE_2D, pingpongColorBuffers[!horizontal]);
     renderQuad();
 }
 
@@ -142,25 +143,25 @@ void Bloom::resize(int newWidth, int newHeight) {
     width_ = newWidth;
     height_ = newHeight;
 
-    glDeleteFramebuffers(1, &hdrFBO);
-    glDeleteTextures(2, colorBuffers);
-    glDeleteRenderbuffers(1, &rboDepth);
-    glDeleteFramebuffers(2, pingpongFBO);
-    glDeleteTextures(2, pingpongColorBuffers);
+    CHECKED_GL_CALL(glDeleteFramebuffers, 1, &hdrFBO);
+    CHECKED_GL_CALL(glDeleteTextures, 2, colorBuffers);
+    CHECKED_GL_CALL(glDeleteRenderbuffers, 1, &rboDepth);
+    CHECKED_GL_CALL(glDeleteFramebuffers, 2, pingpongFBO);
+    CHECKED_GL_CALL(glDeleteTextures, 2, pingpongColorBuffers);
 
     init(newWidth, newHeight, shaderBlur_, shaderFinal_);
 }
 
 void Bloom::begin_frame() {
-    glBindFramebuffer(GL_FRAMEBUFFER, hdrFBO);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    CHECKED_GL_CALL(glBindFramebuffer, GL_FRAMEBUFFER, hdrFBO);
+    CHECKED_GL_CALL(glClear, GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
 void Bloom::end_frame() {
-    glDisable(GL_DEPTH_TEST);
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    CHECKED_GL_CALL(glDisable, GL_DEPTH_TEST);
+    CHECKED_GL_CALL(glBindFramebuffer, GL_FRAMEBUFFER, 0);
     render();
-    glEnable(GL_DEPTH_TEST);
+    CHECKED_GL_CALL(glEnable, GL_DEPTH_TEST);
 }
 
 void Bloom::update_resize() {
